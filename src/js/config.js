f7d49d7902403221b31c971d6a94db6c94a21ebe
Adam Studholme <adam.studholme@version1.com>
2024-04-22 13:25:47 +0100

    test new AJAX

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,6 @@
 //File that contains all the global variables for the project are required throughout the project
-export const FORK_API_KEY = process.env.FORK_API_KEY;
+export const FORK_API_KEY =
+  process.env.FORK_API_KEY || 'd71220a1-064c-42db-b362-e5849593d47c';
 
 //API key for Spoonacular, API stored in 1Password and Netlify
 export const SPOON_API_KEY = process.env.SPOON_API_KEY;
